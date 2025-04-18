8b70bb1dff164691e739a0024c8a92931753a8cf
Pascal Roth <pascal@truestedt.de>
2025-04-18 12:23:00 +0200

    added font awesome for icons

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faUser, faVuejs)
+
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
